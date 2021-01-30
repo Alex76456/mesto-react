28eae8c233f0c41eda6625983f816e430b4cbc1b
Alex76456 <70705720+Alex76456@users.noreply.github.com>
2021-01-30 15:37:49 +0300

    fix: исправить замечания после 1 итерации 10спринт

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,23 @@
 import React from 'react';
 
+function Card({ card, onCardClick }) {
+	function handleClick() {
+		onCardClick(card);
+	}
 
-function Card(props) {
-
-
-  function handleClick() {
-    props.onCardClick(props.card);
-  }
-
-
-  return (
-    <li className="elements__list-item" >
-        <button className="elements__delete-button"></button>
-        <img className="elements__image" src={props.card.link} alt={props.card.name} onClick={handleClick}/>
-        <div className="elements__caption-space">
-            <h2 className="elements__caption">{props.card.name}</h2>
-            <div className="element__like-container">
-                 <button className="elements__caption-like" type="button"></button>
-                 <p className="elements__likes-number">{props.card.likes.length}</p>
-            </div>       
-        </div>
-    </li>
-  )
+	return (
+		<li className="elements__list-item">
+			<button className="elements__delete-button" />
+			<img className="elements__image" src={card.link} alt={card.name} onClick={handleClick} />
+			<div className="elements__caption-space">
+				<h2 className="elements__caption">{card.name}</h2>
+				<div className="element__like-container">
+					<button className="elements__caption-like" type="button" />
+					<p className="elements__likes-number">{card.likes.length}</p>
+				</div>
+			</div>
+		</li>
+	);
 }
 
 export default Card;
